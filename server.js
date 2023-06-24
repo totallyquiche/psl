b623a37d4cb810c5cbd4e9a6ccde9b28b80e48df
Brian Dady <bndady@gmail.com>
2023-06-23 20:34:58 -0500

    Handles redirect from sqlite DB

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 const http = require('http');
+const dbClient = require('sqlite3');
+
+const shortUrlColumnName = process.env.SHORT_COLUMN_NAME
+const longUrlColumNName = process.env.LONG_COLUMN_NAME
+const tableName = process.env.TABLE_NAME
+const sqliteFileName = process.env.SQLITE_FILE_NAME;
+const serverPort = process.env.SERVER_PORT;
+const baseUrl = process.env.BASE_URL;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(302, {
-    'Location': 'https://briandady.com',
+  const db = new dbClient.Database(sqliteFileName);
+
+  const selectStatement = `
+    SELECT \`${shortUrlColumnName}\`, \`${longUrlColumNName}\` \
+    FROM \`${tableName}\` \
+    WHERE \`${shortUrlColumnName}\` = '${req.url}'
+  `;
+
+  db.all(selectStatement, (err, rows) => {
+    if (err) {
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+    } else if (rows.length) {
+      res.writeHead(302, {
+        'Location': `${baseUrl}${rows[0].long}`,
+      });
+    } else {
+      res.writeHead(400, {'Content-Type': 'text/plain'});
+    }
+
+    res.end();
   });
 
-  res.end();
+  db.close();
 });
 
-server.listen(process.env.PORT);
+server.listen(serverPort);
