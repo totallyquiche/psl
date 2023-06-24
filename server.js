93087469a983b9ecaeaa4a2235b234d7db98d1ae
Brian Dady <bndady@gmail.com>
2023-06-24 11:36:04 -0500

    Adds add endpoint to api

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const http = require('http');
 const dbClient = require('sqlite3');
+var querystring = require('querystring');
 
 const shortUrlColumnName = process.env.SHORT_COLUMN_NAME
 const longUrlColumnName = process.env.LONG_COLUMN_NAME
@@ -9,8 +10,40 @@ const tableName = process.env.TABLE_NAME
 const sqliteFileName = process.env.SQLITE_FILE_NAME;
 const serverPort = process.env.SERVER_PORT;
 const baseUrl = process.env.BASE_URL;
+const apiKey = process.env.API_KEY;
 
 const server = http.createServer((req, res) => {
+  if (req.url === '/add' && req.method === 'POST') {
+    let body = '';
+
+    req.on('data', data => {
+      body += data;
+    });
+
+    req.on('end', () => {
+      const db = new dbClient.Database(sqliteFileName);
+      const params = querystring.parse(body);
+
+      if (body.api_key === apiKey) {
+        db.run(
+          `INSERT INTO ${tableName}(\`${longUrlColumnName}\`) VALUES(?)`,
+          [params.post_url],
+          err => {
+            if (err) {
+              console.log(err);
+            }
+          }
+        );
+
+        db.close();
+      }
+    });
+
+    res.end();
+
+    return;
+  }
+
   const db = new dbClient.Database(sqliteFileName);
 
   const selectStatement = `
