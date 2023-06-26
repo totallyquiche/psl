376d3c1807409099b99547e68dc6a42afd1e0960
Brian Dady <bndady@gmail.com>
2023-06-25 21:52:31 -0500

    Removes api for adding slug/url pairs

--- server.js
+++ server.js
@@ -2,56 +2,15 @@ require('dotenv').config()
 
 const http = require('http');
 const dbClient = require('sqlite3');
-var querystring = require('querystring');
 
 const slugColumnName = process.env.SLUG_COLUMN_NAME
 const urlColumnName = process.env.URL_COLUMN_NAME
 const tableName = process.env.TABLE_NAME
 const sqliteFileName = process.env.SQLITE_FILE_NAME;
 const serverPort = process.env.SERVER_PORT;
-const apiKey = process.env.API_KEY;
-const postParamsSlugKey = process.env.POST_PARAMS_SLUG_KEY;
-const postParamsUrlKey = process.env.POST_PARAMS_URL_KEY;
 
 const server = http.createServer((req, res) => {
   const url = new URL(req.url || '', `https://${req.headers.host}`);
-
-  if (
-    req.method === 'POST' &&
-    url.pathname.replace(/\//g, '') === 'add' &&
-    url.searchParams.get('api_key') === apiKey
-  ) {
-    let body = '';
-
-    req.on('data', data => {
-      body += data;
-    });
-
-    req.on('end', () => {
-      const db = new dbClient.Database(sqliteFileName);
-      const params = querystring.parse(body);
-
-      db.run(
-        `INSERT INTO ${tableName}(\`${slugColumnName}\`, \`${urlColumnName}\`) VALUES(?, ?)`,
-        [
-          Buffer.from(params[postParamsSlugKey]).toString('base64'),
-          params[postParamsUrlKey]
-        ],
-        err => {
-          if (err) {
-            console.log(err);
-          }
-        }
-      );
-
-      db.close();
-    });
-
-    res.end();
-
-    return;
-  }
-
   const db = new dbClient.Database(sqliteFileName);
 
   const selectStatement = `
