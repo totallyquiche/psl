aa4872c40bf4e500ad85711421775ab542aa5b1d
Brian Dady <bndady@gmail.com>
2024-06-12 12:40:22 -0500

    test database returns current index

--- src/Database.js
+++ src/Database.js
@@ -1,11 +1,17 @@
 module.exports = class {
+  #index = 0;
   #data = {};
 
   add(key, value) {
     this.#data[key] = value;
+    this.#index++;
   }
 
   load(key) {
     return this.#data[key];
+  }
+
+  getIndex() {
+    return this.#index;
   }
 };
--- tests/Database.test.js
+++ tests/Database.test.js
@@ -1,10 +1,21 @@
 const Database = require("../src/Database");
 
 describe("Database", () => {
+  let database;
+
+  beforeEach(() => (database = new Database()));
+
   test("adds key/value pair", () => {
-    const database = new Database();
     database.add("a", "b");
 
     expect(database.load("a")).toBe("b");
+  });
+
+  test("returns current index", () => {
+    expect(database.getIndex()).toBe(0);
+
+    database.add("a", "b");
+
+    expect(database.getIndex()).toBe(1);
   });
 });
